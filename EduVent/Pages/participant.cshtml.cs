b4501f9aa909eb8a3be77e38dbcb5eda556eb097
7odaZc <s-mahmoud.elzayat@zewailcity.edu.eg>
2024-12-28 23:33:57 +0200

    EduVent Aml

---
+++ EduVent/Pages/participant.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EduVent.Pages
+{
+    public class partcipantModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
